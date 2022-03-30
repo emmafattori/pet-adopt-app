ba54bb79ff6cd207858e946fb1a77c2b176d7bec
Emma Fattori <emma@applydigital.co>
2022-03-30 10:47:57 -0400

    editing fetch url

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -18,7 +18,7 @@ const SearchParams = () => {
     }, [])
 
     async function requestPets() {
-        const res = await fetch(`http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`)
+        const res = await fetch(`https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`)
 
         const json = await res.json()
         setPets(json.pets)
